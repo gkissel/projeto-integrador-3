714d96366ed5d810055a41d42a45255c818c2b4b
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-02 21:58:53 -0300

    gkissel : feat add create organization service

---
+++ apps/api/src/domain/money-app/application/services/organization/create-organization.service.ts
@@ -0,0 +1,49 @@
+import { Either, left, right } from '@/core/either'
+import { Slug } from '@/core/entities/slug'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import Organization from '@/domain/money-app/enterprise/entities/organization'
+
+import OrganizationsRepository from '../../repositories/abstract/organizations.repository'
+import { OrganizationAlreadyExistsError } from '../errors/organization-already-exists-error'
+
+export interface CreateOrganizationServiceRequest {
+  name: string
+  ownerId: string
+  slug: string
+}
+
+type CreateOrganizationServiceResponse = Either<
+  OrganizationAlreadyExistsError,
+  {
+    organization: Organization
+  }
+>
+
+export class CreateQuestionService {
+  constructor(private organizationsRepository: OrganizationsRepository) {}
+
+  async execute({
+    name,
+    ownerId,
+    slug,
+  }: CreateOrganizationServiceRequest): Promise<CreateOrganizationServiceResponse> {
+    const organizationWithSameSlug =
+      await this.organizationsRepository.findBySlug(Slug.createFromText(slug))
+
+    if (organizationWithSameSlug) {
+      return left(new OrganizationAlreadyExistsError('slug', slug))
+    }
+
+    const organization = Organization.create({
+      name,
+      ownerId: new UniqueEntityID(ownerId),
+      slug: Slug.createFromText(slug),
+    })
+
+    await this.organizationsRepository.create(organization)
+
+    return right({
+      organization,
+    })
+  }
+}
