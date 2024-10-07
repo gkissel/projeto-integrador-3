3b6662726f7396e87c046ba3cdead20165dd09dd
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-07 10:48:08 -0300

    gkissel : feat add member entity

---
+++ apps/api/src/domain/money-app/enterprise/entities/member.ts
@@ -0,0 +1,54 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export interface ROLES {
+  ADMIN: 'ADMIN'
+  MEMBER: 'MEMBER'
+}
+
+export interface MemberProps {
+  role: ROLES[keyof ROLES]
+  orgId: UniqueEntityID
+  userId: UniqueEntityID
+
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export class Member extends Entity<MemberProps> {
+  get role() {
+    return this.props.role
+  }
+
+  get orgId() {
+    return this.props.orgId
+  }
+
+  get userId() {
+    return this.props.userId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(
+    props: Optional<MemberProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const organization = new Member(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return organization
+  }
+}
