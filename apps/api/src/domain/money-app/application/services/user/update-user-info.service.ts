ede01cf399c040318c1b98efb3e79a0392bbabf3
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-07 10:47:27 -0300

    gkissel : feat add update user entity service

---
+++ apps/api/src/domain/money-app/application/services/user/update-user-info.service.ts
@@ -0,0 +1,48 @@
+import { Either, left, right } from '@/core/either'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
+import { User } from '@/domain/money-app/enterprise/entities/user'
+
+import { UsersRepository } from '../../repositories/abstract/users.repository'
+
+export interface UpdateUserServiceRequest {
+  userId: string
+  firstName?: string
+  lastName?: string
+  telephone?: string
+}
+
+type UpdateUserServiceResponse = Either<
+  ResourceNotFoundError,
+  {
+    user: User
+  }
+>
+
+export class UpdateQuestionService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    firstName,
+    lastName,
+    telephone,
+    userId,
+  }: UpdateUserServiceRequest): Promise<UpdateUserServiceResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      return left(new ResourceNotFoundError())
+    }
+
+    user.update({
+      firstName,
+      lastName,
+      telephone,
+    })
+
+    await this.usersRepository.update(user)
+
+    return right({
+      user,
+    })
+  }
+}
