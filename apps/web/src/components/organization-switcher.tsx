73db4688627400fd7f6021a09ccbda57a872f061
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-07 21:26:55 -0300

    gkissel : feat add components

---
+++ apps/web/src/components/organization-switcher.tsx
@@ -0,0 +1,83 @@
+import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+
+import { getCurrentOrg } from '@/auth/auth'
+import { getOrganizations } from '@/http/organization/get-organizations'
+
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownDivider,
+  DropdownItem,
+  DropdownLabel,
+  DropdownMenu,
+  DropdownSection,
+} from './catalyst/dropdown'
+import { SidebarItem } from './catalyst/sidebar'
+import { Text } from './catalyst/text'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+
+export async function OrganizationSwitcher() {
+  const currentOrg = getCurrentOrg()
+
+  const { organizations } = await getOrganizations()
+
+  const currentOrganization = organizations.find(
+    (org) => org.slug === currentOrg,
+  )
+
+  return (
+    <Dropdown>
+      <DropdownButton as={SidebarItem}>
+        {currentOrganization ? (
+          <>
+            <Avatar className='size-4'>
+              {currentOrganization.avatarUrl && (
+                <AvatarImage src={currentOrganization.avatarUrl} />
+              )}
+              <AvatarFallback />
+            </Avatar>
+            <Text className='truncate text-left'>
+              {currentOrganization.name}
+            </Text>
+          </>
+        ) : (
+          <Text className='text-xs'>Select organization</Text>
+        )}
+        <ChevronsUpDown className='ml-auto size-4' />
+      </DropdownButton>
+      <DropdownMenu
+        anchor='bottom end'
+        className='ml-4 min-w-64 overflow-hidden rounded-md p-3'
+      >
+        <DropdownSection>
+          <DropdownLabel className='ml-3 pb-2'>Organizations</DropdownLabel>
+
+          {organizations.map((organization) => {
+            return (
+              <DropdownItem
+                key={organization.id}
+                href={`/org/${organization.slug}`}
+              >
+                <Avatar className='mr-2 size-4'>
+                  {organization.avatarUrl && (
+                    <AvatarImage
+                      src={organization.avatarUrl}
+                      title={organization.name}
+                    />
+                  )}
+                  <AvatarFallback />
+                </Avatar>
+                <Text className='line-clamp-1'>{organization.name}</Text>
+              </DropdownItem>
+            )
+          })}
+        </DropdownSection>
+        <DropdownDivider />
+        <DropdownItem href='/create-organization'>
+          <PlusCircle className='mr-2 size-4' />
+          Create new
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
